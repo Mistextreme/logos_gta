0443f301c613d455dded91d41f539a94774988e9
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-11 00:57:53 +0000

    Add links.lua - Generated links file

---
+++ links.lua
@@ -0,0 +1,231 @@
+-- FiveManage Images
+return {
+    ["albany"] = {
+        label = "albany",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/albany.png"
+    },
+    ["annis"] = {
+        label = "annis",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/annis.png"
+    },
+    ["benefactor"] = {
+        label = "benefactor",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/benefactor.png"
+    },
+    ["bf"] = {
+        label = "bf",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/bf.png"
+    },
+    ["bollokan"] = {
+        label = "bollokan",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/bollokan.png"
+    },
+    ["bravado"] = {
+        label = "bravado",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/bravado.png"
+    },
+    ["brute"] = {
+        label = "brute",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/brute.png"
+    },
+    ["buckingham"] = {
+        label = "buckingham",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/buckingham.png"
+    },
+    ["canis"] = {
+        label = "canis",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/canis.png"
+    },
+    ["chariot"] = {
+        label = "chariot",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/chariot.png"
+    },
+    ["cheval"] = {
+        label = "cheval",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/cheval.png"
+    },
+    ["coil"] = {
+        label = "coil",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/coil.png"
+    },
+    ["declasse"] = {
+        label = "declasse",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/declasse.png"
+    },
+    ["dewbauchee"] = {
+        label = "dewbauchee",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/dewbauchee.png"
+    },
+    ["dinka"] = {
+        label = "dinka",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/dinka.png"
+    },
+    ["dundreary"] = {
+        label = "dundreary",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/dundreary.png"
+    },
+    ["emperor"] = {
+        label = "emperor",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/emperor.png"
+    },
+    ["enus"] = {
+        label = "enus",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/enus.png"
+    },
+    ["fathom"] = {
+        label = "fathom",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/fathom.png"
+    },
+    ["gallivanter"] = {
+        label = "gallivanter",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/gallivanter.png"
+    },
+    ["grotti"] = {
+        label = "grotti",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/grotti.png"
+    },
+    ["hijak"] = {
+        label = "hijak",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/hijak.png"
+    },
+    ["hvy"] = {
+        label = "hvy",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/hvy.png"
+    },
+    ["imponte"] = {
+        label = "imponte",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/imponte.png"
+    },
+    ["invetero"] = {
+        label = "invetero",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/invetero.png"
+    },
+    ["jobuilt"] = {
+        label = "jobuilt",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/jobuilt.png"
+    },
+    ["karin"] = {
+        label = "karin",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/karin.png"
+    },
+    ["kraken"] = {
+        label = "kraken",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/kraken.png"
+    },
+    ["lampadati"] = {
+        label = "lampadati",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/lampadati.png"
+    },
+    ["lcc"] = {
+        label = "lcc",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/lcc.png"
+    },
+    ["maibatsu"] = {
+        label = "maibatsu",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/maibatsu.png"
+    },
+    ["mammoth"] = {
+        label = "mammoth",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/mammoth.png"
+    },
+    ["maxwell"] = {
+        label = "maxwell",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/maxwell.png"
+    },
+    ["mtl"] = {
+        label = "mtl",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/mtl.png"
+    },
+    ["nagasaki"] = {
+        label = "nagasaki",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/nagasaki.png"
+    },
+    ["obey"] = {
+        label = "obey",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/obey.png"
+    },
+    ["ocelot"] = {
+        label = "ocelot",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/ocelot.png"
+    },
+    ["overflod"] = {
+        label = "overflod",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/overflod.png"
+    },
+    ["pegassi"] = {
+        label = "pegassi",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/pegassi.png"
+    },
+    ["pfister"] = {
+        label = "pfister",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/pfister.png"
+    },
+    ["principe"] = {
+        label = "principe",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/principe.png"
+    },
+    ["progen"] = {
+        label = "progen",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/progen.png"
+    },
+    ["rune"] = {
+        label = "rune",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/rune.png"
+    },
+    ["schyster"] = {
+        label = "schyster",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/schyster.png"
+    },
+    ["shitzu"] = {
+        label = "shitzu",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/shitzu.png"
+    },
+    ["speedophile"] = {
+        label = "speedophile",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/speedophile.png"
+    },
+    ["stanley"] = {
+        label = "stanley",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/stanley.png"
+    },
+    ["truffade"] = {
+        label = "truffade",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/truffade.png"
+    },
+    ["ubermacht"] = {
+        label = "ubermacht",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/ubermacht.png"
+    },
+    ["vapid"] = {
+        label = "vapid",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/vapid.png"
+    },
+    ["vom-feuer"] = {
+        label = "vom-feuer",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/vom-feuer.png"
+    },
+    ["vulcar"] = {
+        label = "vulcar",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/vulcar.png"
+    },
+    ["vysser"] = {
+        label = "vysser",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/vysser.png"
+    },
+    ["weeny"] = {
+        label = "weeny",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/weeny.png"
+    },
+    ["western"] = {
+        label = "western",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/western.png"
+    },
+    ["willard"] = {
+        label = "willard",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/willard.png"
+    },
+    ["zirconium"] = {
+        label = "zirconium",
+        image = "https://raw.githubusercontent.com/Mistextreme/logos_gta/main/zirconium.png"
+    }
+}
